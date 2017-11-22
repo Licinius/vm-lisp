8fe937b7cc7df11290e29b0fee080dba8de63ddc
ToWaR6 <loic.crauser@free.fr>
2017-11-22 10:13:16 +0100

    update instruction
    suppression de make-vm dans instruction car il est dans machine.lisp

--- machine/intruction.lisp
+++ machine/intruction.lisp
@@ -1,26 +1,3 @@
-; make-vm
-(defun make-vm (nom &optional(taille 250))
-	(set-Symb nom 'mem (make-array taille))
-	(set-Symb nom 'labels (make-hash-table))
-	(set-Symb nom 'SP taille)
-	(set-Symb nom 'RA 0)
-	(set-Symb nom 'PC 0)
-	
-	;Flag
-	(set-Symb nom 'DPG 0)
-	(set-Symb nom 'DEQ 0)
-	(set-Symb nom 'DPP 0)
-
-	(set-Symb nom 'R0 0)
-
-	(set-Symb nom 'inst 0)
-)
-
-; set-Symb
-(defun set-Symb (vm nom val) (setf (get vm nom) val) )
-
-; ------------------------------- VM FUNCTIONS -------------------------------
-
 ; MOVE R1 R2
 (defun MOVE (vm R1 R2)
 	(if (atom R1)
