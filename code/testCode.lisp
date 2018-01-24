d7aa6a946d144ac506bef1f384a1b5243b62850d
ToWaR6 <loic.crauser@free.fr>
2018-01-24 19:26:05 +0100

    reagencement fichier test
    j'ai mis tous les affichages à la fin pour qu'on puisse copier/coller tout le fichier testCode et voir le resultat de chaque fonction sans remonter

--- code/testCode.lisp
+++ code/testCode.lisp
@@ -1,41 +1,51 @@
-(cl-user::quit)
 (require "machine.lisp")
-(format nil "Test factoriel ~%" )
 ;;Ici peuvent commencer les tests
-(make-vm 'vm 1000)
 
-(setf factFunction '((defun fact (n) (if (< n 2) 1 (* n (fact (- n 1))) ) ) (fact 6)))
-
-(compile-load-exec 'vm factFunction)
-(format nil "6! -> Resultat attendu : 720 Resultat obtenu : ~a ~%" (get 'vm 'R0))
-;;========================================;;
-(make-vm 'vm 1000)
-(setf factFunction '((defun fact (n) (if (< n 2) 1 (* n (fact (- n 1))) ) ) (fact 13)))
-(compile-load-exec 'vm factFunction)
-(format nil "13! -> Resultat attendu : 6227020800 Resultat obtenu : ~a ~%" (get 'vm 'R0))
-;;========================================;;
-(make-vm 'vm 1000)
-(setf factFunction '((defun fact (n) (if (< n 2) 1 (* n (fact (- n 1))) ) ) (fact 3)))
-(compile-load-exec 'vm factFunction)
-(format nil "3! -> Resultat attendu : 6 Resultat obtenu : ~a ~%" (get 'vm 'R0))
-;;========================================;;
-
-(format nil "Test fibonacci ~%" )
-(make-vm 'vm 1000)
-(setf fiboFunction '((defun fibo (n) (if (< n 2) n (+ (fibo(- n 1)) (fibo (- n 2) ))  ) ) (fibo 12) ) )
-(compile-load-exec 'vm fiboFunction)
-(format nil "fibo(12) -> Resultat attendu : 144 Resultat obtenu : ~a ~%" (get 'vm 'R0))
-;;========================================;;
-(make-vm 'vm 1000)
-(setf fiboFunction '((defun fibo (n) (if (< n 2) n (+ (fibo(- n 1)) (fibo (- n 2) ))  ) ) (fibo 3) ) )
-(compile-load-exec 'vm fiboFunction)
-(format nil "fibo(3) -> Resultat attendu : 2 Resultat obtenu : ~a ~%" (get 'vm 'R0))
+(setf factFunction6 '((defun fact (n) (if (< n 2) 1 (* n (fact (- n 1))) ) ) (fact 6)))
+(setf factFunction13 '((defun fact (n) (if (< n 2) 1 (* n (fact (- n 1))) ) ) (fact 13)))
+(setf factFunction3 '((defun fact (n) (if (< n 2) 1 (* n (fact (- n 1))) ) ) (fact 3)))
+(setf fiboFunction12 '((defun fibo (n) (if (< n 2) n (+ (fibo(- n 1)) (fibo (- n 2) ))  ) ) (fibo 12) ) )
+(setf fiboFunction3 '((defun fibo (n) (if (< n 2) n (+ (fibo(- n 1)) (fibo (- n 2) ))  ) ) (fibo 3) ) )
+(setf plusMoinsFunction2010 '((defun plus (n m) (+ n m) ) (defun moins (n m) (- n m) ) (plus 5 (moins 20 10)) ) ) 
 
 ;;========================================;;
-(format nil "Test (plus (moins)) ~%" )
 (make-vm 'vm 1000)
-(setf testFunction1 '((defun plus (n m) (+ n m) ) (defun moins (n m) (- n m) ) (plus 5 (moins 20 10)) ) ) 
-(compile-load-exec 'vm testFunction1)
-(format nil "(plus 5 (moins 20 10)) -> Resultat attendu : 15 Resultat obtenu : ~a ~%" (get 'vm 'R0))
+(compile-load-exec 'vm factFunction6)
+(setf fact6 (get 'vm 'R0))
+;;========================================;;
+(make-vm 'vm 1000)
+(compile-load-exec 'vm factFunction13)
+(setf fact13 (get 'vm 'R0))
+;;========================================;;
+(make-vm 'vm 1000)
+(compile-load-exec 'vm factFunction3)
+(setf fact3 (get 'vm 'R0))
+;;========================================;;
+(make-vm 'vm 1000)
+(compile-load-exec 'vm fiboFunction12)
+(setf fibo12 (get 'vm 'R0))
+;;========================================;;
+(make-vm 'vm 1000)
+(compile-load-exec 'vm fiboFunction3)
+(setf fibo3 (get 'vm 'R0))
+;;========================================;;
+(make-vm 'vm 1000)
+(compile-load-exec 'vm plusMoinsFunction2010)
+(setf plusMoins2010 (get 'vm 'R0))
+;;========================================;;
 
 ;;(setf testLoop '( (defun loop (n) ((while (< n 6) (setf n (+ n 1)) ) n ) ) (loop 1) ) )
+
+(setf resTest (format nil "~%Test factoriel ~%"))
+(setf resTest (concatenate 'string resTest (format nil "6! -> Resultat attendu : 720 Resultat obtenu : ~a ~%" fact6)))
+(setf resTest (concatenate 'string resTest (format nil "13! -> Resultat attendu : 6227020800 Resultat obtenu : ~a ~%" fact13)))
+(setf resTest (concatenate 'string resTest (format nil "3! -> Resultat attendu : 6 Resultat obtenu : ~a ~%" fact3)))
+
+(setf resTest (concatenate 'string resTest (format nil "~%Test fibonacci ~%" )))
+(setf resTest (concatenate 'string resTest (format nil "fibo(12) -> Resultat attendu : 144 Resultat obtenu : ~a ~%" fibo12)))
+(setf resTest (concatenate 'string resTest (format nil "fibo(3) -> Resultat attendu : 2 Resultat obtenu : ~a ~%" fibo3)))
+
+(setf resTest (concatenate 'string resTest (format nil "~%Test (plus (moins)) ~%" )))
+(setf resTest (concatenate 'string resTest (format nil "(plus 5 (moins 20 10)) -> Resultat attendu : 15 Resultat obtenu : ~a ~%" plusMoins2010)))
+
+resTest
